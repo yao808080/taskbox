74d76304732c15dca03835f4eae9ccb77edda7d2
takao.yamada <takao.yamada@ulsystems.co.jp>
2023-10-31 15:39:51 +0900

    Simple component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
