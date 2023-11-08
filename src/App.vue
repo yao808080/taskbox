d5a9fcc3e3214d4485ee3eae1c1b5b51df6c2364
takao.yamada <takao.yamada@ulsystems.co.jp>
2023-11-08 13:52:37 +0900

    Simple component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Task from '@/components/Task.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  
   <main>
-    <TheWelcome />
+    <div>DEFAULT </div>
+    <Task :task='{ "id": "1", "title": "Test Task", "state": "TASK_INBOX" }' v-on:pin-task="() => { }"
+      v-on:archive-task="() => { }" />
+    <div>PINNED </div>
+    <Task :task='{ "id": "1", "title": "Test Task", "state": "TASK_PINNED" }' v-on:pin-task="() => { }"
+      v-on:archive-task="() => { }" />
+    <div>ARCHIVED </div>
+    <Task :task='{ "id": "1", "title": "Test Task", "state": "TASK_ARCHIVED" }' v-on:pin-task="() => { }"
+      v-on:archive-task="() => { }" />
   </main>
+
 </template>
 
 <style scoped>
