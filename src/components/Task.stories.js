f9f8df35ffbabdf9d5c17773c1599791bbb1f1f5
takao.yamada <takao.yamada@ulsystems.co.jp>
2023-11-08 16:06:10 +0900

    Simple component

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -4,11 +4,11 @@ import Task from './Task.vue';
 import { action } from '@storybook/addon-actions';
 
 export default {
-  component: Task,
-  title: 'Task',
-  tags: ['autodocs'],
+  component: Task,  // コンポーネント自体
+  title: 'Task',  // Storybook サイドバーでコンポーネントをグループ化または分類する方法
+  tags: ['autodocs'], // コンポーネントのドキュメントを自動的に生成します
   //👇 Our events will be mapped in Storybook UI
-  argTypes: {
+  argTypes: { // 引数を指定
     onPinTask: {},
     onArchiveTask: {},
   },
