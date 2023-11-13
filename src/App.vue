8147ce60dade1755354d0f9fda26e4e4e1a21166
takao.yamada <takao.yamada@ulsystems.co.jp>
2023-11-13 10:50:22 +0900

    Screens

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import Task from '@/components/Task.vue'
-import TaskList from '@/components/TaskList.vue'
+import PureTaskList from '@/components/PureTaskList.vue'
 import Sample from '@/components/Sample.vue'
+import InboxScreen from './components/InboxScreen.vue';
 </script>
 
 <script>
@@ -24,8 +25,8 @@ export default {
 </script>
 
 <template>
-  <main>
-    <div class="task">
+    <main id="app">
+     <div class="task">
       <h2>Task</h2>
       <div>DEFAULT </div>
       <Task :task='{ "id": "1", "title": "Test Task", "state": "TASK_INBOX" }' v-on:pin-task="() => { }"
@@ -41,17 +42,23 @@ export default {
     <div class="taskList">
       <h2>TaskList</h2>
       <div>DEFAULT</div>
-      <TaskList
+      <PureTaskList
         :tasks='[{ "id": "1", "title": "Task 1", "state": "TASK_INBOX" }, { "id": "2", "title": "Task 2", "state": "TASK_INBOX" }, { "id": "3", "title": "Task 3", "state": "TASK_INBOX" }, { "id": "4", "title": "Task 4", "state": "TASK_INBOX" }, { "id": "5", "title": "Task 5", "state": "TASK_INBOX" }, { "id": "6", "title": "Task 6", "state": "TASK_INBOX" }]'
         v-on:pin-task="() => { }" v-on:archive-task="() => { }" />
       <div>With Pinned Tasks</div>
-      <TaskList
+      <PureTaskList
         :tasks='[{ "id": "1", "title": "Task 1", "state": "TASK_INBOX" }, { "id": "2", "title": "Task 2", "state": "TASK_INBOX" }, { "id": "3", "title": "Task 3", "state": "TASK_INBOX" }, { "id": "4", "title": "Task 4", "state": "TASK_INBOX" }, { "id": "5", "title": "Task 5", "state": "TASK_INBOX" }, { "id": "6", "title": "Task 6 (pinned)", "state": "TASK_PINNED" }]'
         v-on:pin-task="() => { }" v-on:archive-task="() => { }" />
       <div>Loading</div>
-      <TaskList :tasks="[]" loading v-on:pin-task="() => { }" v-on:archive-task="() => { }" />
+      <PureTaskList :tasks="[]" loading v-on:pin-task="() => { }" v-on:archive-task="() => { }" />
       <div>Empty</div>
-      <TaskList :tasks="[]" v-on:pin-task="() => { }" v-on:archive-task="() => { }" />
+      <PureTaskList :tasks="[]" v-on:pin-task="() => { }" v-on:archive-task="() => { }" />
+    </div>
+
+    <div class="InboxScreen">
+      <h2>InboxScreen</h2>
+      <div>InboxScreen</div>
+      <InboxScreen/>
     </div>
 
     <div>
@@ -60,11 +67,12 @@ export default {
         <sample v-on:onClick="() => { }" />
         {{ message }}
       </div>
-    </div>
+    </div> 
+    
   </main>
 </template>
 
-<style scoped>
+<style>
 header {
   line-height: 1.5;
 }
